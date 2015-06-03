6ae948b9af3ba9177ed1536c5c999e185cb588a8
Chris Monson <shiblon@gmail.com>
2015-06-03 15:31:37 -0400

    Add ordered lists and update recursion slide.
    The recursion slide is now much beefier, with the intent that it walks
    the student through the concept more slowly. Also, numeric lists are now
    understood using # as the item delineator.

--- static/js/filters.js
+++ static/js/filters.js
@@ -22,6 +22,10 @@ angular.module('runcodeFilters', []).
       return paragraph;
     }
     return function(input) {
+      var list_tags = {
+        '#': 'ol',
+        '-': 'ul',
+      };
       if (input !== undefined && input != "") {
         // Normalize newlines.
         input = input.replace(
@@ -29,15 +33,21 @@ angular.module('runcodeFilters', []).
         // Get paragraphs.
         var ps = input.split("\n\n");
         // Replace list item paragraphs with appropriate markup.
-        var in_list = false;
+        var in_list_tag = false;
         for (i in ps) {
           var paragraph = ps[i];
-          if (ps[i].slice(0, 2) == "- ") {
+          var item_match = paragraph.match(/^([-#]) /);
+          if (item_match) {
             var prefix = "<li>";
             var suffix = "</li>";
-            if (!in_list) {
-              in_list = true;
-              prefix = "<ul>" + prefix;
+            this_list_tag = list_tags[item_match[1]];
+            if (!in_list_tag) {
+              prefix = "<" + this_list_tag + ">" + prefix;
+              in_list_tag = this_list_tag;
+            } else if (in_list_tag != this_list_tag) {
+              // Different kind of list. End this and start the next.
+              prefix = "</" + in_list_tag + ">\n<" + this_list_tag + ">" + prefix;
+              in_list_tag = this_list_tag;
             }
             ps[i] =
               prefix +
@@ -54,9 +64,9 @@ angular.module('runcodeFilters', []).
               prefix = "<pre>\n"
               suffix = "\n</pre>"
             }
-            if (in_list) {
-              in_list = false;
-              prefix = "</ul>\n" + prefix;
+            if (in_list_tag) {
+              prefix = "</" + in_list_tag + ">\n" + prefix;
+              in_list_tag = false;
             }
             ps[i] = prefix + inline_format(paragraph) + suffix;
           }
